d4f27d956fc4e53a23fc9073703b0bd1a397676c
Atharva Dagaonkar <dagaonkaratharva@gmail.com>
2023-06-13 10:55:57 +0530

    Set up HomeTableViewCell and its CollectionView

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -29,13 +29,12 @@ class MainTabBarViewController: UITabBarController {
         vc2.title = "Coming Soon"
         vc3.title = "Top Searches"
         vc4.title = "Downloads"
+        
         tabBar.tintColor = .label
         
         setViewControllers([vc1,vc2,vc3,vc4], animated: true)
         
         
     }
-
-
 }
 
